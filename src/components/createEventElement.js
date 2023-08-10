f12e0e1f32414051e63c3c60b8e5f4c340e74b17
Calin G <calingoia01@yahoo.com>
2023-08-10 15:54:30 +0300

    fixed ticketCategory error

--- src/components/createEventElement.js
+++ src/components/createEventElement.js
@@ -7,6 +7,7 @@ import { useStyle } from './styles';
 import { removeLoader, showLoader } from './removeOrAddLoader';
 
 const createEventElement = (eventData) => {
+  console.log(eventData);
   const addToCartBtnClasses = useStyle('addToCartBtn');
   const inputClasses = useStyle('inputTicket');
   const eventImageMap = { // hardcoded images for events
@@ -18,7 +19,7 @@ const createEventElement = (eventData) => {
     8: './src/assets/rolling_poster.jpeg',
     9: './src/assets/tomorrowland_poster.jpg'
   };
-    const { id, name, description, venue, startDate, endDate /* , ticketCategories */ } = eventData;
+    const { id, name, description, venue, startDate, endDate, ticketCategories } = eventData;
     const eventDiv = document.createElement('div');
     const contentMarkup = `
       <div class="card">
@@ -29,13 +30,18 @@ const createEventElement = (eventData) => {
         <p class="description text-gray-700">Remaining tickets: ${venue.capacity}</p>
         <p class="description text-gray-700">Date: ${startDate} : ${endDate}</p>
         <p class="description text-gray-700">Description: ${description}</p>
+        <label for="ticketCategories">Select a ticket category:</label>
+        <select name="ticketCategories" id="ticketCategories-${id}">
+        <option value="${ticketCategories[0].id}">${ticketCategories[0].description} (Price: ${ticketCategories[0].price}$)</option>
+        <option value="${ticketCategories[1].id}">${ticketCategories[1].description} (Price: ${ticketCategories[1].price}$)</option>
+        </select>
       </div>
     `;
     eventDiv.innerHTML = contentMarkup;
 
     const input = document.createElement('input');
     const addToCart = document.createElement('button');
-    // const selectTicketCategory = document.querySelector('#ticketCategories')
+    
     input.classList.add(...inputClasses);
     input.type = 'number';
     input.min = '0';
@@ -45,8 +51,9 @@ const createEventElement = (eventData) => {
     addToCart.classList.add(...addToCartBtnClasses);
     addToCart.innerText = 'Add ticket to Cart';
     addToCart.addEventListener('click', () => {
-      postOrder(id, /* selectTicketCategory */ input);
-      // console.log(selectTicketCategory.value); // not working
+      const selectTicketCategory = document.querySelector(`#ticketCategories-${id}`)
+      const ticketId = selectTicketCategory.value;
+      postOrder(id, ticketId, input);
     });
 
     eventDiv.appendChild(input);
@@ -54,11 +61,10 @@ const createEventElement = (eventData) => {
     return eventDiv;
 }
 
-const postOrder = (id, /* selectTicketCategory, */ input) => {
-  const numberOfTickets = input.value; 
-  // const ticketCategoryId = selectTicketCategory.value;
+const postOrder = (id, ticketID, input) => {
+  const numberOfTickets = input.value;
+  console.log(id, ticketID, input.value);
   if (parseInt(numberOfTickets)) {
-  console.log(numberOfTickets);
   showLoader();
   fetch('http://localhost:8080/api/v1/orders', {
     method: "POST",
@@ -67,7 +73,7 @@ const postOrder = (id, /* selectTicketCategory, */ input) => {
     },
     body: JSON.stringify({
       eventID: id, // toFix (always selects eventID 1)
-      ticketCategoryID: id, // ticketCategoryId, // todo (implement ticket categories)
+      ticketCategoryID: ticketID, // ticketCategoryId, // todo (implement ticket categories)
       numberOfTickets: +numberOfTickets,
     }),
   }).then((response) => {
