c5cbb0bd919e9afcc63848bd1568519c97409e51
Calin G <calingoia01@yahoo.com>
2023-08-10 11:00:15 +0300

    add createOrderElement, fetchOrders

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /* eslint-disable space-before-function-paren */
 /* eslint-disable semi */
-// Navigate to a specific URL
 import { addEvents } from './src/utils';
+import { createOrderElement } from './src/components/createOrderElement';
 
 function navigateTo(url) {
   history.pushState(null, null, url);
   renderContent(url);
 }
+
 // HTML templates
 function getHomePageTemplate() {
   return `
    <div id="content" >
-      <!--<img src="./src/assets/crowd.jpg" alt="summer" class="homepage-image">-->
       <div class="events flex items-center justify-center flex-wrap space-x-4 space-y-4"">
       </div>
     </div>
@@ -22,6 +22,8 @@ function getOrdersPageTemplate() {
   return `
     <div id="content">
     <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
+      <div class="orders ml-6 mr-6">
+      </div>
     </div>
   `;
 }
@@ -60,6 +62,20 @@ function setupInitialPage() {
   renderContent(initialUrl);
 }
 
+// Get Events
+async function fetchTicketEvents() {
+  const response = await fetch('http://localhost:8080/api/v1/events');
+  const data = await response.json();
+  return data;
+}
+
+// Get Orders
+async function fetchOrders() {
+  const response = await fetch('http://localhost:8080/api/v1/orders?customerID=2');
+  const data = await response.json();
+  return data;
+}
+
 function renderHomePage() {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getHomePageTemplate();
@@ -68,16 +84,18 @@ function renderHomePage() {
   });
 }
 
-// Get Events
-async function fetchTicketEvents() {
-  const response = await fetch('http://localhost:8080/api/v1/events');
-  const data = await response.json();
-  return data;
-}
-
-function renderOrdersPage(categories) {
+function renderOrdersPage() {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getOrdersPageTemplate();
+  const purchasesDiv = document.querySelector('.orders');
+  if (purchasesDiv) {
+    fetchOrders().then((orders) => {
+      orders.forEach((order) => {
+        const newOrder = createOrderElement(order);
+        purchasesDiv.appendChild(newOrder);
+      });
+    })
+  }
 }
 
 // Render content based on URL
---
+++ src/components/createOrderElement.js
@@ -0,0 +1,10 @@
+/* eslint-disable quotes */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable semi */
+import { useStyle } from './styles';
+
+export const createOrderElement = (order) => {
+    
+};
